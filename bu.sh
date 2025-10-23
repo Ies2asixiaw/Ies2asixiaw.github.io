8e0d81d63dd4d2bf70ecf4155ba2b3dd96640409
marnau777 <113452577+marnau777@users.noreply.github.com>
2025-10-24 00:11:28 +0200

    Ponemos un archivo para poderlo descargar
    Añadimos archivo para poderlo derscargar

---
+++ bu.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+sudo touch -t 202509010000 /tmp/fecha_limite
+sudo find /home/ -maxdepth 1 -type d -not -newer /tmp/fecha_limite >> /tmp/usuarios_a_borrar
+ARCHIVO="/tmp/usuarios_a_borrar"
+
+while IFS= read -r LINEA;
+do
+echo $LINEA ########## cambiar echo por rm cuando se verifiquen las fechas ##########
+done < "$ARCHIVO"
+
+sudo rm  /tmp/fecha_limite
+sudo rm /tmp/usuarios_a_borrar
