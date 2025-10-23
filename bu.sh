a930c5320417cb29bd88ea9fa7255099fdcb5e3d
marnau777 <113452577+marnau777@users.noreply.github.com>
2025-10-24 01:04:01 +0200

    Change directory creation to /tmp/centinela

--- bu.sh
+++ bu.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-sudo mkdir centinela.txt
+sudo mkdir /tmp/centinela
 sudo touch -t 202509010000 /tmp/fecha_limite
 sudo find /home/ -maxdepth 1 -type d -not -newer /tmp/fecha_limite >> /tmp/usuarios_a_borrar
 ARCHIVO="/tmp/usuarios_a_borrar"
