539f711e27da5be676e8e4e2583532770fd15fc9
marnau777 <113452577+marnau777@users.noreply.github.com>
2025-10-30 12:55:28 +0100

    Fix formatting in udev rules for Arduino

--- arduino.sh
+++ arduino.sh
@@ -1,4 +1,4 @@
 sudo bash -c 'cat > /etc/udev/rules.d/99-usb-permisos.rules <<EOF
 SUBSYSTEM=="tty", ATTRS{idVendor}=="1a86", ATTRS{idProduct}=="7523", MODE="0666"
-SUBSYSTEM=="tty", ATTRS{idVendor}=="2341", ATTRS{idProduct}=="0043", MODE="0666"
+SUBSYSTEM=="tty", ATTRS{idVendor}=="2341", ATTRS{idProduct}=="0043",MODE="0666"
 EOF'
